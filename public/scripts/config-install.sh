8ba054ed933956339c365ecc0d3b3e976d6f7837
Prabhjot Rai <hello@prabhjotrai.com>
2019-01-26 03:26:23 +0530

    feat: add config install script

--- public/scripts/config-install.sh
+++ public/scripts/config-install.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-echo "Hello world"
+git clone --bare https://github.com/raiprabh/.cfg.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
