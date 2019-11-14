dd3b4adfac420843621d31df4a7f3ff34b04aedf
psr <hello@prabhjotrai.com>
2019-11-14 12:26:41 -0600

    Updates

---
+++ precache-manifest.d1af7c4aeea7ddba47086c814f1ade50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "c978094843580a653843",
+    "url": "/static/css/main.780db5c0.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "4126fda5bf21680d857a",
+    "url": "/static/js/1.4126fda5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "c978094843580a653843",
+    "url": "/static/js/main.c9780948.chunk.js"
+  },
+  {
+    "revision": "89a38d18389bc299e9d2c89534d11a3e",
+    "url": "/static/media/headshot.89a38d18.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "4126fda5bf21680d857a",
+    "url": "/static/css/1.afa94997.chunk.css"
+  },
+  {
+    "revision": "67dc16268a2b063eced0e2eb162632b7",
+    "url": "/index.html"
+  }
+];
